be7117d49b3c99ed6b1dcb285dd08ed4b8331719
VitalMM <vitalcn@qq.com>
2022-05-26 13:36:16 +0800

    shell script

--- pull-all.sh
+++ pull-all.sh
@@ -2,6 +2,7 @@
 # cd ~/
 # git clone git@gitee.com:atlisp/atlisp.git 
 cd ~/atlisp
-git submodule update --init
-cd packages
-git submodule update --init 
+git submodule update --init --recursive
+git submodule foreach git checkout main
+git submodule foreach git pull --rebase origin main
+echo Sync all submodules.
--- sync-all.sh
+++ sync-all.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd ~/atlisp
-
 git submodule sync --recursive
-
+# git submodule update --init --recursive
+# git submodule foreach git checkout main
+# git submodule foreach git pull --rebase origin main
 echo Sync all submodules.
