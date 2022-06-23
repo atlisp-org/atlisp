1573689a9ed2ef3b7c9603ab29ddd9d2c1dde21b
VitalMM <vitalcn@qq.com>
2022-06-24 00:40:03 +0800

    pull all main

--- pull-all.sh
+++ pull-all.sh
@@ -2,6 +2,7 @@
 # cd ~/
 # git clone git@gitee.com:atlisp/atlisp.git 
 cd ~/atlisp
+git pull
 git submodule update --init --recursive
 git submodule foreach git checkout main
 git submodule foreach git pull
