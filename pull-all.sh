d4d02b877fb212c70e0cd106999d280acdcd3db1
VitalMM <vitalcn@qq.com>
2022-06-24 17:03:42 +0800

    root

--- pull-all.sh
+++ pull-all.sh
@@ -4,7 +4,7 @@
 cd ~/atlisp
 git pull
 git submodule update --init --recursive
-git submodule foreach git checkout main
+#git submodule foreach git checkout main
 git submodule foreach git pull
 echo Sync all submodules.
 sleep 5
